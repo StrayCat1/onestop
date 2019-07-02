cca3a9832e9ee34eca080146baad456df8a65f3c
Elliott <elliott.richerson@gmail.com>
2019-07-02 14:23:25 -0600

    add a CesiumTest component to start playing with Cesium

---
+++ client/src/components/cesium/CesiumTest.jsx
@@ -0,0 +1,11 @@
+import { Viewer, Entity } from "resium"
+import { Cartesian3 } from "cesium"
+
+const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100)
+const pointGraphics = { pixelSize: 10 }
+
+export const CesiumTest = () => (
+    <Viewer full>
+        <Entity position={position} point={pointGraphics} />
+    </Viewer>
+)
